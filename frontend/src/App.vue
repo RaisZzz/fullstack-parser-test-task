786eae741fd388de6197b2de9842d49069f8bb6a
RaisZzz <beats.tapping@gmail.com>
2025-06-02 22:58:33 +0300

    [FRONTEND] ADD: Post card

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import {usePostsStore} from "@/stores/posts.ts";
 import {onMounted, ref, type Ref} from "vue";
+import PostCard from "@/components/post/PostCard.vue";
 
 const postsStore = usePostsStore()
 
@@ -8,7 +9,7 @@ const postsQuery: Ref<string> = ref('')
 
 const searchPosts = (): void => {
   if (!postsQuery.value.length) return
-  
+
   postsStore.parseNewPosts(postsQuery.value)
 }
 
@@ -16,7 +17,13 @@ onMounted(postsStore.getPosts)
 </script>
 
 <template>
-  <input v-model="postsQuery" type="text">
-  <button @click="searchPosts"></button>
-  <p>{{ postsStore.posts }}</p>
+  <div class="container">
+    <form @submit.prevent="searchPosts">
+      <input v-model="postsQuery" type="text" placeholder="Поиск...">
+      <input type="submit" value="Найти" />
+    </form>
+    <div class="posts">
+      <PostCard v-for="post in postsStore.posts" :key="post.id" :post="post" />
+    </div>
+  </div>
 </template>
---
+++ frontend/src/components/post/PostCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type {PostModel} from "@/models/post.model.ts";
+import type {PropType} from "vue";
+
+defineProps({
+  post: { type: Object as PropType<PostModel>, required: true }
+})
+</script>
+
+<template>
+  <a class="post" :href="post.link" target="_blank">
+    <p class="post__title">{{ post.title }}</p>
+    <p class="post__description">{{ post.description }}</p>
+    <p class="post__date">{{ post.createdAt.toLocaleDateString() }}</p>
+  </a>
+</template>
+
+<style lang="sass" scoped></style>
